889d9472aaef53d916acca27c0745ca378c7a2d6
thuviengithub0208 <khoaphp@yahoo.com>
2018-08-03 21:02:17 +0700

    "Alert dialog"

--- app/src/main/java/com/ptp/phamtanphat/alertdialog1607/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/alertdialog1607/MainActivity.java
@@ -1,13 +1,52 @@
 package com.ptp.phamtanphat.alertdialog1607;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnAlertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAlertDialog = findViewById(R.id.buttonAlertDialog);
+
+        btnAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertdialog = new AlertDialog.Builder(MainActivity.this);
+                alertdialog.setTitle("Thong bao moi!!!");
+                alertdialog.setMessage("Ban co muon thoat app hay khong?");
+                alertdialog.setIcon(R.mipmap.ic_launcher);
+                alertdialog.setCancelable(false);
+
+                alertdialog.setPositiveButton("Co", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Ban da chon Co", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertdialog.setNegativeButton("Khong", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Ban da chon Khong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertdialog.setNeutralButton("Huy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Ban da chon Huy", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertdialog.show();
+            }
+        });
     }
 }
